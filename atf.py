31da7383170cf02d28fd23fa8656efa4758769ff
Diyar Yermek <diyar24yermek@gmail.com>
2021-07-23 00:29:28 -0700

    Debag of atf

--- atf.py
+++ atf.py
@@ -1,16 +1,18 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-driver = webdriver.Chrome("C:\Apps\chromedriver.exe")
-
-
+driver = webdriver.Chrome("/usr/local/bin/chromedriver")
 
 percent =[]
 driver.get("https://www.atfbank.kz/phys/deposits")
-print(driver.title)
 content = driver.page_source
 soup = BeautifulSoup(content)
 for a in soup.findAll(attrs={'class':'product__data'}):
     name=a.find('span', attrs={'class':'product__string'})
     percent.append(name.text)
 print(percent)
+
+
+driver.get("https://www.vtb-bank.kz/individuals/srochnye-vklady/vklad-sberegatelnyy/")
+percent = []
+vtb=driver.find_element_by_class_name("")
