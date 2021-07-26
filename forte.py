a9971d82ed961ea69bacb00e5d7a03c131caa827
Diyar Yermek <diyar24yermek@gmail.com>
2021-07-26 00:32:01 -0700

    added halyk parsing to forte.py

--- forte.py
+++ forte.py
@@ -6,11 +6,10 @@ import pandas as pd
 driver = webdriver.Chrome("/usr/local/bin/chromedriver")
 
 
-driver.get("https://bank.forte.kz/deposits")
+
 #driver.implicitly_wait(20)
 
-content = driver.page_source
-soup = BeautifulSoup(content, "html.parser")
+
 
 
 today = date.today() # сегодняшнаяя дата 
@@ -25,14 +24,48 @@ percent =[] # процент ставки
 
 
 #FORTEBank
+driver.get("https://bank.forte.kz/deposits")
+content = driver.page_source
+soup = BeautifulSoup(content, "html.parser")
+
+
 data.append(today.strftime("%d/%m/%Y"))
 bank.append("ForteBank")
 deposit.append("в приложении")
-sroch.append("срочный")
+sroch.append("несрочный")
 popol.append("с пополнениями")
 
 forte = soup.find_all("td", "MuiTableCell-root MuiTableCell-body")
 percent.append((forte[10].find('span')).text)
+#FORTEBank
+
+
+#HALYKBank
+driver.get("https://halykbank.kz/deposit/srochnye-vklady")
+content = driver.page_source
+soup = BeautifulSoup(content, "html.parser")
+
+
+data.append(today.strftime("%d/%m/%Y"))
+bank.append("HalykBank")
+deposit.append("Универсальный")
+sroch.append("несрочный")
+popol.append("с пополнениями")
+
+halyk1 = soup.find("div", "swiper-slide swiper-slide-visible swiper-slide-active")
+percent.append(halyk1.find('div', attrs={'class':'item-num'}).text)
+
+
+data.append(today.strftime("%d/%m/%Y"))
+bank.append("HalykBank")
+deposit.append("Максимальный")
+sroch.append("сберегательный")
+popol.append("без пополнения")
+
+halyk1 = soup.find("div", "swiper-slide swiper-slide-visible swiper-slide-next")
+percent.append(halyk1.find('div', attrs={'class':'item-num'}).text)
+
+#HALYKBank
 
 '''
 for a in soup.findAll(attrs={'class':'product__data'}):
@@ -49,4 +82,3 @@ for a in soup.findAll(attrs={'class':'product__data'}):
 
 df = pd.DataFrame({'Дата':data, 'БВУ':bank, 'Депозит':deposit, 'Сроч/несроч': sroch, 'C/без попол.': popol, 'процент':percent}) 
 df.to_csv('BVU.csv', index=False, encoding='utf-8')
-#FORTEBank
